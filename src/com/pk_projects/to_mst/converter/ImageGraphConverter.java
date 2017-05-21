b6c3225adfbecec5e64b045697716547190114ee
dariusz.matoga <dariusz.matoga@gmail.com>
2017-05-21 11:38:08 +0200

    add empty implementation for converters and factory

---
+++ src/com/pk_projects/to_mst/converter/ImageGraphConverter.java
@@ -0,0 +1,12 @@
+package com.pk_projects.to_mst.converter;
+
+import com.pk_projects.to_mst.structure.Graph;
+
+import java.io.File;
+
+public class ImageGraphConverter implements GraphConverter {
+    @Override
+    public File convert(Graph graph, String path) {
+        return null;
+    }
+}
---
+++ src/com/pk_projects/to_mst/converter/XMLGraphConverter.java
@@ -0,0 +1,12 @@
+package com.pk_projects.to_mst.converter;
+
+import com.pk_projects.to_mst.structure.Graph;
+
+import java.io.File;
+
+public class XMLGraphConverter implements GraphConverter {
+    @Override
+    public File convert(Graph graph, String path) {
+        return null;
+    }
+}

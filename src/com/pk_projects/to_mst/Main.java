75f556cd8bca865962b256d6327dd93fe41b16ee
dariusz.matoga <dariusz.matoga@gmail.com>
2017-05-21 13:32:12 +0200

    prepare integration

--- src/com/pk_projects/to_mst/Main.java
+++ src/com/pk_projects/to_mst/Main.java
@@ -1,5 +1,8 @@
 package com.pk_projects.to_mst;
 
+import com.pk_projects.to_mst.algorithm.KruskalMST;
+import com.pk_projects.to_mst.algorithm.MST;
+import com.pk_projects.to_mst.algorithm.PrimMST;
 import com.pk_projects.to_mst.converter.GraphConverter;
 import com.pk_projects.to_mst.converter.ImageGraphConverter;
 import com.pk_projects.to_mst.converter.XMLGraphConverter;
@@ -13,9 +16,19 @@ public class Main {
         GraphFactory graphFactory = new XMLGraphFactory();
         GraphConverter imageGraphConverter = new ImageGraphConverter();
         GraphConverter xmlGraphConverter = new XMLGraphConverter();
+        MST kruskalMST = new KruskalMST();
+        MST primMST = new PrimMST();
 
         Graph graph = graphFactory.create();
+        Graph primGraph = primMST.getMST(graph);
+        Graph kruskalGraph = kruskalMST.getMST(graph);
 
-        System.out.println(graph);
+        imageGraphConverter.convert(graph, "graph.png");
+        imageGraphConverter.convert(primGraph, "graphPrim.png");
+        imageGraphConverter.convert(kruskalGraph, "graphKruskal.png");
+
+        xmlGraphConverter.convert(graph, "graph.xml");
+        xmlGraphConverter.convert(primGraph, "graphPrim.xml");
+        xmlGraphConverter.convert(kruskalGraph, "graphKruskal.xml");
     }
 }
